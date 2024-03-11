057cbd562f19c274740a6e622cd5d11ef2b29aa9
CGMGT <cgarcia@cgmsoluciones.com>
2024-03-10 23:19:19 -0600

    version inicial del proyecto fastCrud

---
+++ src/main/java/com/example/fastCrud/FastCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.fastCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastCrudApplication.class, args);
+	}
+
+}
